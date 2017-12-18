98275c6d4393e232623aec13619af175e7424192
changyaqian <1214906384@qq.com>
2017-12-18 21:06:53 +0800

    权限控制部分代码

--- seckill/src/main/java/com/cz/seckill/seckill/database/model/SysUser.java
+++ seckill/src/main/java/com/cz/seckill/seckill/database/model/SysUser.java
@@ -4,4 +4,31 @@ package com.cz.seckill.seckill.database.model;
  * Created by changyaqian on 2017/12/18.
  */
 public class SysUser {
+    private int id;
+    private String userName;
+    private String passWord;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
--- seckill/src/main/java/com/cz/seckill/seckill/mapper/UserMapper.java
+++ seckill/src/main/java/com/cz/seckill/seckill/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package com.cz.seckill.seckill.mapper;
 
 import com.cz.seckill.seckill.database.model.SysUser;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by changyaqian on 2017/12/18.
  */
 public interface UserMapper {
-    SysUser findByUserName(String username);
+    SysUser findByUserName(@Param("userName") String userName);
 }
