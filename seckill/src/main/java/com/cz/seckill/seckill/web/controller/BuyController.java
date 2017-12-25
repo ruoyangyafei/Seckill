3d7a1a30faf11db6a6474111d6b12fa1938d57dd
changyaqian <1214906384@qq.com>
2017-12-25 21:44:20 +0800

    权限控制部分

---
+++ seckill/src/main/java/com/cz/seckill/seckill/web/controller/BuyController.java
@@ -0,0 +1,8 @@
+package com.cz.seckill.seckill.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BuyController extends BaseController {
+
+}
---
+++ seckill/src/main/java/com/cz/seckill/seckill/web/domain/BuyOrder.java
@@ -0,0 +1,10 @@
+package com.cz.seckill.seckill.web.domain;
+
+/**
+ * 封装订单信息领域对象
+ * Created by changyaqian on 2017/12/19.
+ */
+public class BuyOrder {
+    //订单uuid
+    private String orderUuid;
+}
