ad5f1d4e651a20d135b54acd840eb1540f7a196b
changyaqian <1214906384@qq.com>
2018-01-03 20:28:13 +0800

    .

--- seckill/src/main/java/com/cz/seckill/seckill/web/controller/BuyController.java
+++ seckill/src/main/java/com/cz/seckill/seckill/web/controller/BuyController.java
@@ -1,8 +1,19 @@
 package com.cz.seckill.seckill.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BuyController extends BaseController {
+    @Autowired
+    TaskExecutor executor;
+    public void test(){
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
 
+            }
+        });
+    }
 }
